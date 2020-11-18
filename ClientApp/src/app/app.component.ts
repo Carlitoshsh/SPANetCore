a07737cc19843844bb0e84686c9e5f5a20ba5664
Carlos Gómez <caragomezher@unal.edu.co>
2020-11-18 02:37:09 +0000

    Add sw to angular!

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { SwUpdate } from "@angular/service-worker";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "app-root",
+  templateUrl: "./app.component.html",
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = "app";
+  updateAvailable: boolean = false;
+
+  constructor(private update: SwUpdate) {}
+
+  ngOnInit() {
+    console.log("🎨", this.update);
+    if (this.update.isEnabled) {
+      this.update.available.subscribe(() => {
+        this.updateAvailable = true;
+      });
+    }
+  }
+
+  updateNow() {
+    document.location.reload();
+    console.log("The app is updating right now");
+  }
 }
